86c64a07d04fb7982b70e8e9f6b6306ee2666f70
unknown <ompreetam317@gmail.com>
2020-09-23 20:24:18 +0530

    [Preetam]Added : Added Welcom Message

---
+++ Welcome .java
@@ -0,0 +1,22 @@
+
+public class Welcome{
+	public static final int PLAYER1_START = 0;
+	public static final int PLAYER2_START = 0;
+	
+
+	public static void main(String[] args) {
+        	System.out.println("Welcome to Sanake and ladder problem");
+		
+		
+
+
+		
+	 	}
+
+
+
+
+
+
+
+}
